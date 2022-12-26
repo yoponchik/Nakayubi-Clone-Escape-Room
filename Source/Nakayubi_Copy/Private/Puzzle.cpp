e1c13ecc5bf096d5375079dfb40ce1d9bc510ac4
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-12-26 16:47:45 +0900

    Create Puzzle Script

---
+++ Source/Nakayubi_Copy/Private/Puzzle.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Puzzle.h"
+
+// Sets default values
+APuzzle::APuzzle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//set default values of arrays
+	//intialize puzzleSolved = false;
+	for (int32 i = 0; i < puzzlePart.Num(); i++) {
+		puzzlePart[i] = false;
+
+		puzzleStatus[i] = false;
+	}
+
+}
+
+// Called when the game starts or when spawned
+void APuzzle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APuzzle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+
+	//if(clicked), then true
+	//if unclicked, then false
+
+	//if hint1 && hint2 && hint3 ,etc
+		//puzzleSolved = true
+
+	puzzleSolved = CheckPuzzleStatus();
+}
+
+bool APuzzle::CheckPuzzleStatus(){
+	for (int32 i = 0; i < puzzlePart.Num(); i++) {
+		if (puzzlePart[i] == puzzleSolution[i]) {
+			puzzleStatus[i] = true;
+			return false;
+		}
+	}
+
+	return true;
+}
---
+++ Source/Nakayubi_Copy/Public/Puzzle.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Puzzle.generated.h"
+
+UCLASS()
+class NAKAYUBI_COPY_API APuzzle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APuzzle();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	//bool arrays
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PuzzleSettings)
+	TArray<bool> puzzleSolution;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = PuzzleSettings)
+	TArray<bool> puzzlePart;
+
+	//bool puzzleSolved = false
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = PuzzleSettings)
+	TArray<bool> puzzleStatus;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = PuzzleSettings)
+	bool puzzleSolved;
+
+	bool CheckPuzzleStatus();
+};
