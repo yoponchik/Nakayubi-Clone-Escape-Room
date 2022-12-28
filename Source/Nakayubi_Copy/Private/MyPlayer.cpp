f7bda0d4301e0e6de89769d6a422c2b266fc8f67
mysk423 <57009810+yoponchik@users.noreply.github.com>
2022-12-28 19:51:47 +0900

    Update MyPlayer Script
    -Click Feature
    -now can see if an actor of specific class was clicked

---
+++ Source/Nakayubi_Copy/Private/MyPlayer.cpp
@@ -0,0 +1,82 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPlayer.h"
+#include "Kismet/GameplayStatics.h"
+#include "Puzzle1.h"
+
+
+// Sets default values
+AMyPlayer::AMyPlayer()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+	playerController = UGameplayStatics::GetPlayerController(this, 0);
+	playerController->bShowMouseCursor = true;
+
+	GetWorld()->GetFirstPlayerController()->SetShowMouseCursor(true);
+}
+
+// Called every frame
+void AMyPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FHitResult hitResult;
+	const ETraceTypeQuery TraceChannel = UEngineTypes::ConvertToTraceType((ECC_Visibility));
+
+	//If Hit result has the channel trace channel, if it is hit result...
+	if (playerController->GetHitResultUnderCursorByChannel(TraceChannel, true, hitResult)) {
+		if (isClicked) {
+		//Get the actor
+			clickedActor = hitResult.GetActor();
+#pragma region Debug
+			//Get Actor Name
+			//UE_LOG(LogTemp, Warning, TEXT("%s"), *clickedActor->GetName());						//Need asterisk because need pointer to print out character
+#pragma endregion
+			//if puzzle 1 is clicked
+			if (clickedActor->IsA<APuzzle1>()) {
+			//if (Cast<APuzzle1>(clickedActor)) {									//Either works
+#pragma region Debug
+
+				UE_LOG(LogTemp, Warning, TEXT("iS A PUZZLE1"));						//Need asterisk because need pointer to print out character
+#pragma endregion
+				APuzzle1* puzzle1 = Cast<APuzzle1>(clickedActor); 									//Either works
+				puzzle1->ChangeMaterialColor();
+			}
+			//change material
+		}
+	}
+}
+
+// Called to bind functionality to input
+void AMyPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Click", IE_Pressed, this, &AMyPlayer::Click);
+	PlayerInputComponent->BindAction("Click", IE_Released, this, &AMyPlayer::UnClick);
+}
+
+void AMyPlayer::Click() {
+	isClicked = true;
+#pragma region Debug
+	//UE_LOG(LogTemp, Warning, TEXT("Click"));
+#pragma endregion
+}
+
+void AMyPlayer::UnClick() {
+	isClicked = false;
+#pragma region Debug
+	//UE_LOG(LogTemp, Warning, TEXT("Click End"));
+#pragma endregion
+
+}
